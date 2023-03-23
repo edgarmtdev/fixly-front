b68765a8578201b64ed205a73cbba49546c4816f
edgarmt.dev <edgarmontiel961@gmail.com>
2023-03-22 18:09:43 -0600

    feat(fetch): reviews from backend and rendering in UI

--- modules/products/components/reviews/index.jsx
+++ modules/products/components/reviews/index.jsx
@@ -8,27 +8,30 @@ const Reviews = ({ reviews, loading }) => {
       <Title>Reviews ({reviews?.length})</Title>
       <FormReview />
       <section className=" mt-10 w-3/4">
-        <div className="">
-          <div className="flex gap-5 items-center">
-            <p className=" text-lg font-semibold">Good Product</p>
-            <span className=" text-xs text-gray-400">9 month ago</span>
-          </div>
-          <div>
-            {" "}
-            <span>⭐⭐⭐⭐⭐</span>
-          </div>
-          <div className="mt-4">
-            <p>
-              The best product, recommended for beginners in the world of
-              electronics, easy to use and resistant materials.
-            </p>
-          </div>
-          <div className="mt-6 flex gap-6 text-primary_variant-400 font-semibold">
-            <span className=" cursor-pointer hover:underline">Like</span>
-            <span className=" cursor-pointer hover:underline">Dislike</span>
-            <span className=" cursor-pointer hover:underline">Reply</span>
-          </div>
-        </div>
+        {reviews?.map((review, index) => {
+          return (
+            <div key={index}>
+              <div className="flex gap-5 items-center">
+                <p className=" text-lg font-semibold">{review.title}</p>
+                <span className=" text-xs text-gray-400">
+                  {review.date?.toLocaleString()}
+                </span>
+              </div>
+              <div>
+                {" "}
+                <span>⭐⭐⭐⭐⭐</span>
+              </div>
+              <div className="mt-4">
+                <p>{review.comment}</p>
+              </div>
+              <div className="mt-6 flex gap-6 text-primary_variant-400 font-semibold">
+                <span className=" cursor-pointer hover:underline">Like</span>
+                <span className=" cursor-pointer hover:underline">Dislike</span>
+                <span className=" cursor-pointer hover:underline">Reply</span>
+              </div>
+            </div>
+          );
+        })}
       </section>
     </div>
   );
