902d0dd28ea44fbeebb66f1d18769cb24f4c1bfc
edgarmt.dev <edgarmontiel961@gmail.com>
2023-03-24 13:26:09 -0600

    refactor(module): styling with tailwind

--- modules/cart/card/styled.jsx
+++ modules/cart/card/styled.jsx
@@ -3,4 +3,6 @@ import styled from "styled-components";
 export const Product = styled.section`
   display: flex;
   gap: 1rem;
+  justify-content: space-between;
+  align-items: center;
 `;
--- modules/cart/index.jsx
+++ modules/cart/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useSelector } from "react-redux";
 import SpinnerLoader from "../../components/utils/loaders/spinner";
 import Card from "./card";
 import { BuyButton, Container, NotProducts, Title } from "./styles";
-import { AiOutlineShoppingCart } from "react-icons/ai";
 
 export default function MyCart({ products }) {
   const { total, loading } = useSelector((state) => state.cart);
@@ -19,14 +19,21 @@ export default function MyCart({ products }) {
         <AiOutlineShoppingCart size={40} />
         <p>Shop cart</p>
       </Title>
-      <div className=" p-8">
+      <div className="p-8">
         {products.length > 0 ? (
           <>
-            {products.map((item) => (
-              <Card key={item.product._id} data={item} />
-            ))}
-            <p>total: ${total} </p>
-            <BuyButton>Buy now</BuyButton>
+            {" "}
+            <div>
+              {products.map((item) => (
+                <Card key={item.product._id} data={item} />
+              ))}
+              <div className="my-10 ml-auto flex">
+                <p className=" text-2xl ml-auto">Total: ${total} </p>
+              </div>
+            </div>
+            <div className="flex mt-10 pt-10">
+              <BuyButton>Buy now</BuyButton>
+            </div>
           </>
         ) : (
           <NotProducts>Not products</NotProducts>
