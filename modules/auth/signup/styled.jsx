7b322e816b102ad2d36a0fe04744548d6de7feb9
edgarmt.dev <edgarmontiel961@gmail.com>
2023-03-24 23:13:26 -0600

    refactor(component): styles in card to signup module

--- modules/auth/signup/styled.jsx
+++ modules/auth/signup/styled.jsx
@@ -1,25 +1,25 @@
 import styled from "styled-components";
+import BREAKPOINTS_DEVICES from "../../../theme/global/breakpoints";
 
 export const Container = styled.div`
   position: relative;
-  width: 100vw;
-  height: 380px;
+  width: 90%;
+  min-height: 600px;
   margin: 3rem 1.25rem;
   background-color: #f6f6f6;
   padding: 0.5rem;
   border-radius: 2px;
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-  @media (min-width: 800px) {
-    height: 650px;
-    margin-left: 5rem;
-    margin-right: 5rem;
-    display: grid;
+
+  @media ${BREAKPOINTS_DEVICES.tablet} {
+    width: 80%;
   }
 
-  @media (min-width: 800px) {
-    width: 900px;
-    margin-left: auto;
-    margin-right: auto;
+  @media ${BREAKPOINTS_DEVICES.laptop} {
+    max-width: 1024px;
+    height: 500px;
+    margin-left: 5rem;
+    margin-right: 5rem;
   }
 `;
 
