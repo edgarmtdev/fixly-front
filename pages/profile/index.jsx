fd1e154ae00bb170e338ebc3fa5ce5140a690952
edgarmt.dev <edgarmontiel961@gmail.com>
2023-03-31 12:57:45 -0600

    feat(pages): add pages to user info

---
+++ pages/profile/index.jsx
@@ -0,0 +1,13 @@
+import { HeadComponent } from "components/utils/head";
+import React from "react";
+
+const Profile = () => {
+  return (
+    <>
+      <HeadComponent title={"Profile | Fixly"} />
+      <div>Profile</div>
+    </>
+  );
+};
+
+export default Profile;
---
+++ pages/profile/reviews/index.jsx
@@ -0,0 +1,12 @@
+import { HeadComponent } from "components/utils/head";
+
+const Reviews = () => {
+  return (
+    <>
+      <HeadComponent title={"Reviews | Fixly"} />
+      <div>Reviews</div>
+    </>
+  );
+};
+
+export default Reviews;
