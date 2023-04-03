ae4a818f6c051805452a3b648e70dc333f46319c
edgarmtdev <edgarmontiel961@gmail.com>
2023-04-03 05:29:02 +0200

    test new pc

--- context/ProductsContext.js
+++ context/ProductsContext.js
@@ -6,7 +6,7 @@ const productsMockup = data;
 export const productsContext = createContext();
 
 export default function ProductsContext({ children }) {
-  const [products, setProducts] = useState(productsMockup);
+  const [products] = useState(productsMockup);
 
   return (
     <productsContext.Provider value={{ products: products }}>
