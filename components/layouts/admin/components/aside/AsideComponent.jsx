5ff8f94e3697dec00f49acea5f99d4dc9a6e00a3
edgarmtdev <edgarmontiel961@gmail.com>
2023-06-28 21:53:49 -0600

    refactor(aside): responsive design to bar aside

--- components/layouts/admin/components/aside/AsideComponent.jsx
+++ components/layouts/admin/components/aside/AsideComponent.jsx
@@ -10,15 +10,15 @@ export default function AsideComponent() {
     <>
       {view && (
         <div
-          className="absolute z-10 top-0 left-0 bg-[#00000078] h-[100vh] w-[100%]"
+          className="absolute z-10 top-0 left-0 bg-[#00000078] h-[100vh] w-[100%] tabletL:hidden"
           onClick={() => setView(false)}
         ></div>
       )}
       <aside
-        className={`bg-primary-100 h-[100vh] top-0 shadow z-30 ${
+        className={`bg-primary-100 h-[100vh] top-0 shadow z-30 w-[300px] ${
           view
             ? "translate-x-[0%] transition-all duration-200"
-            : "translate-x-[-100%] transition-all duration-200"
+            : "translate-x-[-150%] transition-all duration-200"
         } tabletL:translate-x-0 absolute tabletL:flex flex-col tabletL:static`}
       >
         <TitleAside />
