302e4d7d129c238bca73b06ae669158a5cdcf270
edgarmtdev <edgarmontiel961@gmail.com>
2023-07-03 14:02:14 -0600

    resolve(error): imports of components

--- pages/admin/details/[id].jsx
+++ pages/admin/details/[id].jsx
@@ -1,6 +1,6 @@
 import AdminLayout from "components/layouts/admin/Admin";
 import { useRouterQuery } from "hooks";
-import DetailsToAdmin from "modules/admin/modules/details";
+import DetailsToAdmin from "modules/admin/pages/details";
 
 export default function DetailsProductAdmin() {
   const query = useRouterQuery("id");
